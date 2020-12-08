79d270eb516e87b014c40dac75d632846f82c36e
CasLeeflang <466933@student.fontys.nl>
2020-12-08 09:26:36 +0100

    .

---
+++ BoulderBeta.Context/BoulderContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using BoulderBeta.Models;
+
+
+
+namespace BoulderBeta.Context
+{
+    public class BoulderContext : DbContext
+    {
+        public BoulderContext (DbContextOptions<BoulderContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Boulder> Boulders { get; set; }
+    }
+
+}
--- BoulderBeta/Startup.cs
+++ BoulderBeta/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using BoulderBeta.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace BoulderBeta
 {
@@ -24,6 +26,9 @@ namespace BoulderBeta
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<BoulderContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("BoudlerContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
