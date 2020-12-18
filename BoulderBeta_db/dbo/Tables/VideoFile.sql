752a0bc8f9bc2500c2b8ebaed7d32773a0796868
CasLeeflang <466933@student.fontys.nl>
2020-12-18 11:45:42 +0100

    Added video model and db

---
+++ BoulderBeta_db/dbo/Tables/VideoFile.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[VideoFile]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	Vname varchar(100) NOT NULL,
+	Vpath varchar(200) NOT NULL
+)
---
+++ DataLibrary/Models/VideoModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLibrary.Models
+{
+    public class VideoModel
+    {
+        public string Vname { get; set; }
+        public string Vpath { get; set; }
+    }
+}
