787f9d78e7581e52df24aa44d0ee425919261b45
CasLeeflang <466933@student.fontys.nl>
2020-12-18 12:54:39 +0100

    Upload video works, serperate from post upload, now combine

--- BoulderBeta/Controllers/HomeController.cs
+++ BoulderBeta/Controllers/HomeController.cs
@@ -72,7 +72,12 @@ namespace BoulderBeta.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult UploadVideo()
+        {
+            return View();
+        }
 
-        
+
+
     }
 }
---
+++ BoulderBeta/Controllers/VideoController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Web;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace BoulderBeta.MVC.Controllers
+{
+    public class VideoController : Controller
+    {      
+
+        [HttpPost("SaveVideo")]
+        public async Task<IActionResult> SaveVideo(List<IFormFile> videoFiles, int boulderId, int userId)
+        {
+            var size = videoFiles.Sum(f => f.Length);
+
+            var filePaths = new List<string>();
+            foreach(var formFile in videoFiles)
+            {
+                var filePath = Path.Combine(Directory.GetCurrentDirectory()+"/VideoUploads/", formFile.FileName);
+                filePaths.Add(filePath);
+
+                using( var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await formFile.CopyToAsync(stream);
+                }
+            }
+            
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
